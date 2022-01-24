e732e4f6274345146caf825e033376f3504916e7
Amir Hosein Baqinejad <ambaqinejad@gmail.com>
2022-01-24 23:07:13 +0330

    prepare project structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
